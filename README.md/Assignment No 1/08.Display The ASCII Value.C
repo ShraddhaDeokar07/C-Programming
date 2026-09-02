8ec3cafbf29745894e0047cce8068405322634b7
ShraddhaDeokar07 <shraddhadeokar1387@gmail.com>
2026-09-02 22:07:07 +0530

    Add files via upload

---
+++ README.md/Assignment No 1/08.Display The ASCII Value.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch;
+
+    printf("\n Enter the character = ");
+    scanf("%c",&ch);
+
+    printf("\n The ASCII value is %c = %d",ch , ch);
+
+    getch();
+    return 0;
+}
