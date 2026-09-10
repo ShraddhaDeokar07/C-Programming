3becf16201a12283677066de894554514f776887
ShraddhaDeokar07 <shraddhadeokar1387@gmail.com>
2026-09-10 21:19:25 +0530

    Add files via upload

---
+++ Assignment No 01/16.Fahrenhiet Into Celsisu.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+    int fahrenhiet = 0.0 , cel = 0.0 , temp;
+
+    printf("\n Enter a tempreture =");
+    scanf("%d",&temp);
+
+    1 celsisu = fahrenhiet-32 * 5/9;
+
+    printf("\n The tempreture is  %d = %d",temp);
+
+    getch();
+    return 0;
+}
+
+
