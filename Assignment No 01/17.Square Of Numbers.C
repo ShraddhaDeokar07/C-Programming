88ac2f60e0e2d073da69d0c433b8ddccfbde6db3
ShraddhaDeokar07 <shraddhadeokar1387@gmail.com>
2026-09-10 21:19:47 +0530

    Add files via upload

---
+++ Assignment No 01/17.Square Of Numbers.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num = 0 , square;
+
+    printf("\n Enter The Number = ");
+    scanf("%d",&Num);
+
+    (square = Num * Num);
+
+    printf("\n Number of square is:%d",square);
+
+    getch();
+    return 0;
+
+}
