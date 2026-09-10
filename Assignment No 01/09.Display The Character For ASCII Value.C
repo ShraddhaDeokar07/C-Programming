6dbf7567c048fa8c25441cf83aa635db5990bcb8
ShraddhaDeokar07 <shraddhadeokar1387@gmail.com>
2026-09-10 21:10:00 +0530

    Add files via upload

---
+++ Assignment No 01/09.Display The Character For ASCII Value.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ASCII;
+
+    printf("\n Enter The ASCII Value = ");
+    scanf("%d",&ASCII);
+
+    printf("\n The Charater for ASCII value %d is = %c ", ASCII,ASCII);
+
+    getch();
+    return 0;
+}
