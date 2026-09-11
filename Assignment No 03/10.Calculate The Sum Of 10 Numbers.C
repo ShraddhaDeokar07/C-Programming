9e6c879c5ea73f1b1742b75af2f179e2a08f2fe6
ShraddhaDeokar07 <shraddhadeokar1387@gmail.com>
2026-09-11 21:44:10 +0530

    Add files via upload

---
+++ Assignment No 03/10.Calculate The Sum Of 10 Numbers.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i, num, sum = 0;
+
+    printf("\n Enter Number = ");
+
+    for(i = 1; i <= 10; i++)
+
+    {
+       scanf(" %c",&num)
+       sum = sum + num;
+    }
+
+
+}
