2135767673beac6d64de18242aaaf44d129f28d5
ShraddhaDeokar07 <shraddhadeokar1387@gmail.com>
2026-09-11 21:39:55 +0530

    Add files via upload

---
+++ Assignment No 03/05.Display Numbers Multiple Times.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int n1,n2,i;
+
+     printf("\n Enter First Number = ");
+     scanf("%d",&n1);
+
+     printf("\n Enter Second Number = ");
+     scanf("%d",&n2);
+
+     for(i = 1; i <= n2; i++)
+     {
+         printf("\n %d",n1);
+     }
+     getch();
+     return 0;
+}
