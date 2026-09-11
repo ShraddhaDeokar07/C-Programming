3ae44505a0e604befa9353175578ebe64da90031
ShraddhaDeokar07 <shraddhadeokar1387@gmail.com>
2026-09-11 21:57:37 +0530

    Add files via upload

---
+++ Assignment No 04/06.Print Reverse Repeated number.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0, j = 0 , irow = 0, jcol = 0;
+
+    printf("\n Enter row number : ");
+    scanf("%d",&irow);
+
+    printf("\n Enter col number : ");
+    scanf("%d",&jcol);
+
+    for(i = irow; i >= 1; i--)
+    {
+        for(j = 1; j <= jcol; j++)
+        {
+            printf(" %d ",i);
+        }
+        {
+            printf("\n");
+        }
+    }
+    getch();
+    return 0;
+}
