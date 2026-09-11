80739f4c8ae9badd361ac5e20fd138df45eb7d9e
ShraddhaDeokar07 <shraddhadeokar1387@gmail.com>
2026-09-11 21:18:42 +0530

    Add files via upload

---
+++ Assignment No 02/09.Prints The Corresponding Month Name.C
@@ -0,0 +1,90 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0;
+    printf("\n Enter month number = ");
+    scanf("%d",&No);
+
+       if(No == 1)
+
+           {
+            printf("This month is : January");
+
+           }
+       else if(No == 2)
+
+            {
+                printf("This month is : February");
+            }
+
+       else if(No == 3)
+
+            {
+                 printf("This month is : March");
+            }
+
+       else if(No == 4)
+
+            {
+                printf("This month is : April");
+            }
+
+       else if(No == 5)
+
+
+            {
+                printf("This month is : May");
+            }
+
+       else if(No == 6)
+
+            {
+                printf("This month is : June");
+            }
+        else if(No == 7)
+
+            {
+                printf("This month is : July");
+            }
+
+       else if(No == 8)
+
+            {
+                printf("This month is : August");
+            }
+
+      else if(No == 9)
+
+            {
+                printf("This month is : september");
+            }
+
+      else if(No == 10)
+
+            {
+                printf("This month is : Octomber");
+            }
+
+      else if(No == 11)
+
+            {
+                printf("This month is : November");
+            }
+
+      else if(No == 12)
+
+            {
+                printf("This month is : December");
+            }
+
+        else
+            {
+                printf("\n Invalid Month Number ");
+            }
+
+        getch();
+        return 0;
+
+}
