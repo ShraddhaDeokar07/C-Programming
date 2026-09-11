c2d6358e51d06cda1eb3596239d996f3448506bb
shraddhaa <shraddhadeokar1387@gmail.com>
2026-09-11 22:22:39 +0530

    Add files via upload

---
+++ Assignment No 05/03.Print A To Z In Triangle Pattern.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0 , j = 0 , rc = 0;
+    char ch = 'A';
+
+    printf("\n Enter The r & c ");
+    scanf("%d",&rc);
+
+    for(i=0; i<=rc; i++)
+    {
+        for(j=0; j<=rc, i; j++)
+        {
+            printf(" ");
+        }
+    }
+
+}
