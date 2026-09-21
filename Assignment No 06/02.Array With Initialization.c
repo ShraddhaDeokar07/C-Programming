6ef9ef19af65e5e5809d9a58456aa4f81255a52a
shraddhaa <shraddhadeokar1387@gmail.com>
2026-09-21 21:55:23 +0530

    Add files via upload

---
+++ Assignment No 06/02.Array With Initialization.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+       int a[5] = {24,11,30,44,500};
+
+    printf("\n 1st Element Value is = %d",a[0]);
+    printf("\n 2nd Element Value is = %d",a[1]);
+    printf("\n 3rd Element Value is = %d",a[2]);
+    printf("\n 4th Element Value is = %d",a[3]);
+    printf("\n 5th Element Value is = %d",a[4]);
+
+    getch();
+    return 0;
+
+}
